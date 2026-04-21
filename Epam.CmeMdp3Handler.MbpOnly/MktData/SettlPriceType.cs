a501c6130ee11144c543e5d4ef91862e76f61d43
djj_mingie <junjie.ding@mingie.cn>
2026-04-21 09:58:20 +0800

    Fix snapshot receive bugs.

--- Epam.CmeMdp3Handler.MbpOnly/MktData/SettlPriceType.cs
+++ Epam.CmeMdp3Handler.MbpOnly/MktData/SettlPriceType.cs
@@ -1,20 +1,15 @@
 namespace Epam.CmeMdp3Handler.MktData
 {
-    public static class SettlPriceType
-    {
-        public const short Nothing = 0;
-        public const short Final = 1;
-        public const short Actual = 1 << 1;
-        public const short Rounded = 1 << 2;
-        public const short Intraday = 1 << 3;
-        public const short NullValue = 1 << 7;
+    using System;
 
-        public static bool HasIndicator(int flags, int flag) => (flags & flag) == flag;
-        public static bool IsNothing(byte indicator) => indicator == Nothing;
-        public static bool HasFinal(byte indicator) => HasIndicator(indicator, Final);
-        public static bool HasActual(byte indicator) => HasIndicator(indicator, Actual);
-        public static bool HasRounded(byte indicator) => HasIndicator(indicator, Rounded);
-        public static bool HasIntraday(byte indicator) => HasIndicator(indicator, Intraday);
-        public static bool HasNullValue(byte indicator) => HasIndicator(indicator, NullValue);
+    [Flags]
+    public enum SettlPriceType
+    {
+        Nothing = 0,
+        Final = 1,
+        Actual = 1 << 1,
+        Rounded = 1 << 2,
+        Intraday = 1 << 3,
+        NullValue = 1 << 7,
     }
 }
